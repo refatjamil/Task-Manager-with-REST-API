cced9f2a660c2311b0d2de40e66ae78dc949974b
rifat jamil <rifatjamil54@gmail.com>
2023-09-25 02:22:26 +0600

    Work on Task App - Task create, read, update, delete with Class based View - Show all details with multiple photos - Marks as complete options

--- task_manager/tasks/admin.py
+++ task_manager/tasks/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Task, Task_Img
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'title', 'description', 'c_time', 'u_time', 'd_time', 'priority', 'mark']
+admin.site.register(Task, TaskAdmin)
+
+
+class TaskImgAdmin(admin.ModelAdmin):
+    list_display = ['id', 'task_id', 'task_title', 'img']
+
+    @admin.display(ordering="task__title")
+    def task_title(self, obj):
+        return obj.task.title
+    
+    @admin.display(ordering="task__id")
+    def task_id(self, obj):
+        return obj.task.id
+admin.site.register(Task_Img, TaskImgAdmin)
+
+    
--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -3,5 +3,13 @@ from . import views
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('', login_required(views.Task.as_view()), name='tasks'),
+    path('', login_required(views.TaskListView.as_view()), name='tasks'),
+    path('create_task/', login_required(views.TaskCreateView.as_view()), name='create_task'),
+    path('more_details/<int:pk>/', login_required(views.TaskDetailsView.as_view()), name='more_details'),
+    path('update_task/<int:pk>/', login_required(views.TaskUpdateView.as_view()), name='update_task'),
+    path('delete/<int:pk>/', login_required(views.TaskDeleteView.as_view()), name='task_delete'),
+    path('add_photo/<int:id>/', views.add_photo, name='add_photo'),
+    path('delete_photo/<int:id>/', views.delete_photo, name='delete_photo'),
+    path('complete/<int:id>/', views.complete, name='complete'),
+
 ]
